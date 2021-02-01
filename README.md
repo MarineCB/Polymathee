f1b9fa3f358cf32910aacdb3fbb50a54a2dc628e
Nicolas Thong <thong.nicolas@hotmail.com>
2021-02-01 12:18:04 +0100

    Setup Init Project Front/Server - POL-2

---
+++ README.md
@@ -0,0 +1,5 @@
+Start Front Project : 
+    - cd frontend && npm start
+
+Start Server Project :
+    - ./mvnw spring-boot:run
---
+++ src/test/java/com/polymathee/polymathee/PolymatheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polymathee.polymathee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolymatheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
