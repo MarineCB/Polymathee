9bf734a0c84dc33e45092cfdebd44391d48a474a
françois <francoisgillioen@live.fr>
2021-02-01 18:03:17 +0100

    POL-6 #Comment design for Login a Registration (to improve and isolate into components)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card, FormControl, Grid, TextField  } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import { Typography } from '@material-ui/core';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Card raised="true" style={{ borderRadius: 30 }}>
+        <FormControl style={{margin:20}}>
+          <Grid container justify="left">
+            <Typography style={{marginLeft: 10}}>Inscription </Typography>
+          </Grid>
+          <Grid spacing={2}>
+            <TextField style={{padding: 10}} placeholder="Nom">Nom</TextField>
+            <TextField style={{padding: 10}} placeholder="Prenom"></TextField>
+          </Grid>
+            <TextField style={{padding: 10}} placeholder="Identifiant"></TextField>
+            <TextField style={{padding: 10}} placeholder="Mot de passe"></TextField>
+            <TextField style={{padding: 10}} placeholder="Addresse mail"></TextField>
+            <TextField style={{padding: 10}} placeholder="Confirmer addresse mail"></TextField>
+            <Link style={{ fontSize: 17}} >Se connecter</Link>
+            <Grid container justify="flex-end">
+            <Button style={{ borderRadius: 50, width: '50px', color: 'white', backgroundColor: "#252C41"}}>OK</Button>
+            </Grid>
+        </FormControl>
+        </Card>
+        <Card raised="true" style={{ marginTop:30, borderRadius: 30 }}>
+        <FormControl style={{width:450, margin:20}}>
+          <Grid container justify="left">
+            <Typography style={{marginLeft: 10}}>Connexion </Typography>
+          </Grid>
+            <TextField style={{padding: 10}} placeholder="Identifiant"></TextField>
+            <TextField style={{padding: 10}} placeholder="Mot de passe"></TextField>
+
+            <Link style={{ fontSize: 17}} >S'inscrire</Link>
+            <Grid container justify="flex-end">
+            <Link style={{ fontSize: 17}} >Mot de passe oublié ? </Link>
+            <Button style={{ borderRadius: 50, width: '50px', color: 'white', backgroundColor: "#252C41"}}>OK</Button>
+            </Grid>
+        </FormControl>
+        </Card>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +59,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
