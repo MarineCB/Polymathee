30946bab0a7294c495c7cd2835187239f327d3aa
françois <francoisgillioen@live.fr>
2021-02-02 17:21:10 +0100

    POL-6 #Comment Tweaked design, and started publication page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Card, FormControl, Grid, TextField  } from '@material-ui/core';
+import { Button, Card, FormControl, Grid, TextField,Chip, IconButton, InputAdornment, List  } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 import { Typography } from '@material-ui/core';
-
+import {Add, CloudUpload} from '@material-ui/icons'
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="Center-cards">
         <img src={logo} className="App-logo" alt="logo" />
         <Card raised="true" style={{ borderRadius: 30 }}>
         <FormControl style={{margin:20}}>
@@ -29,21 +29,92 @@ function App() {
             </Grid>
         </FormControl>
         </Card>
-        <Card raised="true" style={{ marginTop:30, borderRadius: 30 }}>
-        <FormControl style={{width:450, margin:20}}>
+        <Card raised="true" style={{ borderRadius: 10, marginTop: 20 }}>
+        <FormControl style={{margin:20}}>
           <Grid container justify="left">
-            <Typography style={{marginLeft: 10}}>Connexion </Typography>
+            <Typography style={{marginLeft: 10}}>Créer une publication </Typography>
           </Grid>
-            <TextField style={{padding: 10}} placeholder="Identifiant"></TextField>
-            <TextField style={{padding: 10}} placeholder="Mot de passe"></TextField>
-
-            <Link style={{ fontSize: 17}} >S'inscrire</Link>
-            <Grid container justify="flex-end">
-            <Link style={{ fontSize: 17}} >Mot de passe oublié ? </Link>
-            <Button style={{ borderRadius: 50, width: '50px', color: 'white', backgroundColor: "#252C41"}}>OK</Button>
+            <TextField style={{padding: 10}} placeholder="Titre"></TextField>
+            <TextField style={{padding: 10}} placeholder="Description"></TextField>
+          <Grid container>
+            <Grid style={{paddingInline: 10}}>
+              <Typography style={{padding: 10}} >Tag principal</Typography>
+              <Card style={{padding: 30}}>
+              <Chip style={{marginInline:2, backgroundColor: '#528FF8'}} label="Maths"></Chip>
+              <Chip style={{marginInline:2, backgroundColor: '#528FF8'}} label="Biologie"></Chip>
+            </Card>
+            </Grid>
+            <Grid>
+              <Typography style={{padding: 10}} >Tags supplémentaires</Typography>
+              <Card style={{padding: 30}}>
+                <Grid > 
+                <Chip style={{marginInline:2, backgroundColor: '#528FF8'}} label="EFREI"></Chip>
+                <Chip style={{marginInline:2, backgroundColor: '#528FF8'}}  label="Long à lire"></Chip>
+                </Grid>
+                <div>
+        <TextField
+        style= {{margin:30}}
+          id="outlined-adornment-amount"
+          variant="outlined"
+          label="Ajouter un nouveau tag"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">+</InputAdornment>,
+          }}
+        />
+                </div>
+              </Card>
+            </Grid>
+          </Grid>
+            <Button
+        variant="contained"
+        color="default"
+        style = {{margin: 20}}
+        startIcon={<CloudUpload />}
+      >Ajouter une pièce jointe </Button>
+            <Grid container justify="center" style={{marginTop:20}}>
+              <Button style={{ borderRadius: 50, marginInline:2 ,color: 'white', backgroundColor: "#528FF8", textTransform:'none'}}>Enregistrer</Button>
+              <Button style={{ borderRadius: 50, marginInline:2 ,color: 'white', backgroundColor: "#528FF8", textTransform:'none'}}>Publier</Button>
             </Grid>
         </FormControl>
         </Card>
+        <Grid direction='column' container style={{background:'#116466'}}>
+          <Typography style={{fontSize:30}}>Publications</Typography>
+          <Card>
+          </Card>
+          <List>
+            <Card style={{padding:10, margin:20}}>
+              <Grid container item direction='row'>
+                <Grid item xs={3}>
+                  <Button variant="contained"/>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button variant="contained"/>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button variant="contained"/>
+                </Grid>
+              </Grid>
+            </Card>
+            <Card style={{padding:10, margin:20}}>
+              <Grid container item>
+              <Grid item xs={3}>
+                <Typography >Efrei Formal Modelling</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                <Typography>Sauvegarder un brouillon</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                <Button variant="contained" style={{background:'orange'}}/>
+                <Button variant="contained" style={{background:'white'}}/>
+                <Button variant="contained" style={{background:'white'}}/>
+                </Grid>
+              </Grid>
+            </Card>
+          </List>
+          <Typography  style={{fontSize:30}}>Favoris</Typography>
+          <Card>
+          </Card>
+        </Grid>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -55,7 +126,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
